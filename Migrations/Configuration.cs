45edb3247d990a0ef98f772721817dc9e03977d9
diazpierrie <ezequieldiazpierri@gmail.com>
2021-07-12 19:34:10 -0300

    EF Migration

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace tesdtParcial2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Domain.Data.AfiliadoDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Domain.Data.AfiliadoDbContext";
+        }
+
+        protected override void Seed(Domain.Data.AfiliadoDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
